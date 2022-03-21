8dd24e7513498abef3c232a8f571c1a5bc444b25
Ignaty Kashnitsky <noir.ik@gmail.com>
2022-03-21 14:28:43 +0100

    end of lesson

---
+++ types/app.js
@@ -0,0 +1,69 @@
+// console.log('Hello from app.js in head');
+
+// let a = 10;
+// a = a / 2;
+
+// const b = 3.14156;
+// // b = b - 0.00006;
+// console.log(b);
+
+// var  - Не используется в последних версиях JS. Используем let
+
+// get_Som
+
+// let get = 5;
+// let som = 3;
+// let res = get - som;
+
+let str = 'Vasya';
+// let hello = 'Hello';
+// let helloStr = hello + ', ' + str; // concatination
+// console.log(helloStr);
+// let helloStr_2 = `${hello}, ${str}`; // interpolation
+// console.log(helloStr_2);
+
+// console.log(typeof helloStr_2);
+
+// let a = 5.1;
+// console.log(typeof a);
+
+// let isDone = true;
+// console.log(typeof isDone);
+
+// console.log('5' + '5');
+// console.log(1 + '1');
+// console.log(typeof +'11');
+// console.log(typeof Number('11'));
+// console.log(typeof parseInt('11.1'));
+// console.log(parseInt('11.9'));
+// console.log(typeof parseFloat('11.9'));
+// console.log(parseFloat('11.9'));
+
+// console.log(typeof String(11));
+// console.log(typeof (11 + ''));
+// let number = 10;
+// console.log(typeof number.toString());
+
+// console.log(Boolean(null)); // false - 'false', 0, '', undefined, null
+
+// console.log(!!!!!5);
+
+// console.log(!!str);
+
+// NaN - Not a Number
+
+// console.log(5 % 2);
+
+let a = 0,
+	b = 4;
+
+// let c = a + b;
+// c = c + b // c += b ... * / -
+
+// a++ // a = a + 1 ... a += 1
+// ++a
+// a-- or --a
+
+// console.log(`Всего просмотров ${++a}`);
+console.log(`Всего просмотров до нашего посещения ${a++}, `);
+console.log(`А после него - ${a}`);
