427a8699a6fdd21137866c0280f0b7bb80d6f44b
Ignaty Kashnitsky <noir.ik@gmail.com>
2022-03-30 12:52:56 +0200

    end of lesson

---
+++ app.js
@@ -0,0 +1,41 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// console.log(numbers[7]);
+const words = ['one', 'two', 'three'];
+
+// console.log(words.length);
+// console.log(words[0]);
+// console.log(words[1]);
+// console.log(words[2]);
+// for (let index = 0; index < words.length; index++) {
+// 	console.log(words[index]);
+// }
+
+// for (let i = 0; i < numbers.length; i++) {
+// 	if (numbers[i] % 2 === 0) {
+// 		console.log(numbers[i]);
+// 	}
+// }
+
+// for (let i = 0; i < numbers.length; i++) {
+// 	if (numbers[i] === 2) {
+// 		console.log(numbers[i] ** 2);
+// 	}
+// }
+
+// const juri = [true, true, false, true, false];
+// let count = 0;
+// for (let i = 0; i < juri.length; i++) {
+// 	if (juri[i]) {
+// 		count++;
+// 	}
+// }
+
+// console.log(count);
+
+// const user = [8, 8, 8, 8, 8];
+// let base = 30;
+// let resultSum = 0;
+// for (let i = 0; i < user.length; i++) {
+// 	resultSum += user[i]; // resultSum = resultSum + user[i]
+// }
+// console.log(resultSum * base * 4 * 12);
