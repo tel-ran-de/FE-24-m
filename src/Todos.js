91a1cf454316c5b416269e652e7fff565d77c394
Nolra <nolraprof@gmail.com>
2022-06-10 15:10:23 +0400

    Todos

---
+++ src/Todos.js
@@ -0,0 +1,31 @@
+// https://todomvc.com/examples/react/#/
+// создать module.css для Todos и использовать его
+function Todos() {
+
+
+  // 1. input field, который будет принимать ввод от пользователя
+  // и событие onKeyPress (погуглить и найти использование этого события в react)
+  // 1.2 Вынести input field в отдельный компонент (useState - поднятие состояний)
+  // Хранить состояние input field в Todos
+
+
+  // 2. Сделать TodosList компонентом и рендерить(мапить) в него state
+  // Хранить состояние TodosList в Todos (массив объектов)
+
+
+  // дополнительно
+  // 3. Как обновить state TodoList? Можно использовать useEffect c слежением за 
+  // состоянием введенного (enter event) input field
+
+
+
+  return (
+    <div className="todos">
+
+
+
+    </div>
+  );
+}
+
+export default Todos;
